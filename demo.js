dd2e7beca4d27da9fc24cba851a05145192cec16
Hiroaki Yasutomo <yasutomo@shiftbrain.co.jp>
2019-02-12 15:31:29 +0900

    demo

--- demo.js
+++ demo.js
@@ -12,12 +12,28 @@ css(div, {
   top: (document.documentElement.clientHeight - pxSize) / 2,
 })
 
+var pxSize2 = 10
+var div2 = document.createElement('div')
+document.body.appendChild(div2)
+css(div2, {
+  width: pxSize2,
+  height: pxSize2,
+  background: 'red',
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  zIndex: 1,
+})
+
 var pinch = require('./')(window)
 
 var scale = 1
 
-window.addEventListener('touchstart', function (ev) {
-  ev.preventDefault() // no scrolling
+// window.addEventListener('touchstart', function (ev) {
+//   ev.preventDefault() // no scrolling
+// })
+document.addEventListener('gesturestart', function (ev) {
+  ev.preventDefault() // no zoom
 })
 
 pinch.on('start', function () {
@@ -28,8 +44,12 @@ pinch.on('end', function () {
   css(div, 'background', 'blue')
 })
 
-pinch.on('change', function (current, prev) {
+pinch.on('change', function (current, prev, center) {
   var delta = (current - prev) * 0.01
   scale += delta
   css(div, 'transform', 'scale(' + scale.toFixed(5) + ')')
+  css(div2, {
+    left: (center[0] - pxSize2 * 0.5) + 'px',
+    top: (center[1] - pxSize2 * 0.5) + 'px',
+  }
 })
